2768f097575e2bcbd6c1197b44723332463709a3
Dimitri Justeau <dimitri.justeau@gmail.com>
2012-11-06 14:21:56 +0100

    DAO : J'ai besoin de récupérer tous les vols d'un jour car c'est super cool !!

--- Projet-d-option-GIPAD-Amadeus/src/dao/DAO.java
+++ Projet-d-option-GIPAD-Amadeus/src/dao/DAO.java
@@ -1,6 +1,13 @@
 package dao;
 
+import java.util.List;
+
+import model.Flight;
+
 public interface DAO {
 
-	
+	/**
+	 * @return Tous les vols partant le jour dd du mois MM de l'anée yy
+	 */
+	List<Flight> getAllFlights(String yyMMdd);
 }
