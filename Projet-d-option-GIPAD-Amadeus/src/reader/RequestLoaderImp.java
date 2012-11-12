435a6be223e75db5ffc0e6e58c95d49bf51652a4
Pierre Chouin <chouin.pierre@gmail.com>
2012-11-12 16:46:30 +0100

    implémentation de requestLoader !

--- Projet-d-option-GIPAD-Amadeus/src/reader/RequestLoaderImp.java
+++ Projet-d-option-GIPAD-Amadeus/src/reader/RequestLoaderImp.java
@@ -2,6 +2,7 @@ package reader;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -10,32 +11,78 @@ import context.userConstraints.cve.CVE;
 import context.userConstraints.cvf.CVF;
 import context.userConstraints.cvo.CVO;
 
+/**
+ * implémentation de l'interface RequestLoader, 
+ * qui permet de charger des fichiers de requêtes
+ * @author Pierre Chouin
+ *
+ */
 public class RequestLoaderImp implements RequestLoader {
 
+    /**
+     * double-point
+     */
     public static final String DOUBLE_POINT = ":";
     
+    /**
+     * caractère du commentaire
+     */
     public static final String COMMENTAIRE = "#";
     
+    /**
+     * caractère du séparateur de dates
+     */
     public static final String SEPARATEUR = ",";
     
+    /**
+     * String utilisée pour représenter une contrainte sur la ville de départ
+     */
     public static final String CVO="CVO";
     
+    /**
+     * String utilisée pour représenter une contrainte sur la ville d'arrivée
+     */
     public static final String CVF="CVF";
     
+    /**
+     * String utilisée pour représenter une contrainte sur une ville-étape
+     */
     public static final String CVE="CVE";
     
+    /**
+     * String utilisée pour représenter une contrainte générale
+     */
     public static final String CG="CG";
     
+    /**
+     * taille dans le fichier du nom simple des contraintes
+     */
     public static final int TAILLE_CV=3;
     
-    public static final int TAILLE_CG=2;
-    
+    /**
+     * Contraintes sur la ville d'origine
+     */
     private CVO cvo;
     
+    /**
+     * COntraintes sur la ville d'arrivée
+     */
     private CVF cvf;
+    
+    /**
+     * liste de contraintes sur les villes-étapes
+     */
+    private List<CVE> cves;
+    
+    /**
+     * contraintes générales
+     */
+    private List<CG> cgs;
     
     @Override
     public void loadRequest(final String dir) {
+        cves = new ArrayList<CVE> ();
+        cgs = new ArrayList<CG> ();
         Scanner sc;
         try {
             sc = new Scanner(new File(dir));
@@ -46,36 +93,40 @@ public class RequestLoaderImp implements RequestLoader {
                 if(s.substring(0, TAILLE_CV).equals(CVO)){
                     String CVO0 = getStringPropre(s);
                     s=sc.nextLine();
-                    String CVO10 = getStringPropre(s).split(SEPARATEUR)[0];
-                    String CVO11 = getStringPropre(s).split(SEPARATEUR)[1];
+                    String[] CVO1 = getStringPropre(s).split(SEPARATEUR);
                     s=sc.nextLine();
-                    String CVO20 = getStringPropre(s).split(SEPARATEUR)[0];
-                    String CVO21 = getStringPropre(s).split(SEPARATEUR)[1];
-                    cvo= new CVO(CVO0, CVO10, CVO11, CVO20, CVO21);
+                    String[] CVO2 = getStringPropre(s).split(SEPARATEUR);
+                    cvo= new CVO(CVO0, CVO1, CVO2);
                 }
                 
                 if(s.substring(0, TAILLE_CV).equals(CVE)){
                     s=sc.nextLine();
                     String CVE0 = getStringPropre(s);
                     s=sc.nextLine();
-                    String CVO10 = getStringPropre(s).split(SEPARATEUR)[0];
-                    String CVO11 = getStringPropre(s).split(SEPARATEUR)[1];
+                    String CVE1s = getStringPropre(s);
+                    boolean CVE1=true;
+                    if(Integer.parseInt(CVE1s)==0){ CVE1=false; }
                     s=sc.nextLine();
-                    String CVO20 = getStringPropre(s).split(SEPARATEUR)[0];
-                    String CVO21 = getStringPropre(s).split(SEPARATEUR)[1];
-                    cvo= new CVO(CVO0, CVO10, CVO11, CVO20, CVO21);
-                    
+                    String[] CVE2 = getStringPropre(s).split(SEPARATEUR);
+                    s=sc.nextLine();
+                    String[] CVE3s = getStringPropre(s).split(SEPARATEUR);
+                    int [] CVE3 = {Integer.parseInt(CVE3s[0]), 
+                            Integer.parseInt(CVE3s[1])};
+                    s=sc.nextLine();
+                    String[] CVE4 = getStringPropre(s).split(SEPARATEUR);
+                    s=sc.nextLine();
+                    int CVE5 = Integer.parseInt(getStringPropre(s));
+                    CVE cve= new CVE(CVE0, CVE1, CVE2, CVE3, CVE4, CVE5);
+                    cves.add(cve);          
                 }
                 
                 if (s.substring(0, TAILLE_CV).equals(CVF)){
                     String CVF0 = getStringPropre(s);
                     s=sc.nextLine();
-                    String CVF10 = getStringPropre(s).split(SEPARATEUR)[0];
-                    String CVF11 = getStringPropre(s).split(SEPARATEUR)[1];
+                    String[] CVF1 = getStringPropre(s).split(SEPARATEUR);
                     s=sc.nextLine();
-                    String CVF20 = getStringPropre(s).split(SEPARATEUR)[0];
-                    String CVF21 = getStringPropre(s).split(SEPARATEUR)[1];
-                    cvf= new CVF(CVF0, CVF10, CVF11, CVF20, CVF21);
+                    String[] CVF2 = getStringPropre(s).split(SEPARATEUR);
+                    cvf= new CVF(CVF0, CVF1, CVF2);
                 }
             }
             
@@ -95,23 +146,26 @@ public class RequestLoaderImp implements RequestLoader {
 
     @Override
     public CVF getCVF() {
-        // TODO Auto-generated method stub
-        return null;
+        return cvf;
     }
 
     @Override
     public List<CVE> getCVEs() {
-        // TODO Auto-generated method stub
-        return null;
+       return cves;
     }
 
     @Override
     public List<CG> getCGs() {
-        // TODO Auto-generated method stub
-        return null;
+        return cgs;
     }
 
-    private String getStringPropre(String s){
+    /**
+     * retourne la String sans commentaires, sans la contrainte avant, 
+     * et sans espaces, ni avant, ni après
+     * @param s la String que l'on veut modifier
+     * @return la String modifiée
+     */
+    private String getStringPropre(final String s){
         return s.split(DOUBLE_POINT)[1].split(COMMENTAIRE)[0].trim();
     }
 
