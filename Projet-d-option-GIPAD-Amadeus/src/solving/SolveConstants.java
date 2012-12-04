954d8f5ee362e05b2c40da09b6c7519db11f2336
papigrou <marc.nkaoua@gmail.com>
2012-12-04 11:12:19 +0100

    Ajout de classe constantes dans le package solving

---
+++ Projet-d-option-GIPAD-Amadeus/src/solving/SolveConstants.java
@@ -0,0 +1,21 @@
+package solving;
+
+public final class SolveConstants {
+
+    /**
+     * L'option des Variable
+     */
+    public static final String VARIABLES_OPTION = "cp:enum";
+    
+    /**
+     * La granularité de l'échelle de temps, ici 5 minutes soit 300 000 ms.
+     */
+    static final int GRANULARITE = 300000;
+
+    /**
+     * Le nombre de miliseconds dans une heure.
+     */
+    static final int NB_MS_IN_ONE_HOUR = 3600000;
+    
+    
+}
